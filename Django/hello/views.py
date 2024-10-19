f1e59fcaf0b99d0283ff9e8e318c77ea3ea56fba
davibsua <147537715+davibsua@users.noreply.github.com>
2024-10-19 15:54:16 +0100

    todos los archivos de django

---
+++ Django/hello/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request): 
+    return render(request, "hello/index.html")
+
+def david(request): 
+    return HttpResponse("Hello, David")
+
+def brian(request): 
+    return HttpResponse("Hello, Brian")
+
+
+def greet(request, name): 
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
